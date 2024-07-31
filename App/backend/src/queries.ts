ffea751169573e803988da62dd6afe24a1d67ebd
d-pettersson <dp@artificialrome.com>
2024-07-31 11:47:45 +0200

    feat(queries): add tone of voice query

--- App/backend/src/queries.ts
+++ App/backend/src/queries.ts
@@ -1,13 +1,14 @@
-// export const identity = ```
-// PREFIX ns1: <http://superside.org/burger-king#>
-// PREFIX gpt: <http://www.ontotext.com/gpt/>
-//
-// SELECT ?subject ?predicate ?object ?query WHERE {
-//     ?subject ?predicate ?object .
-//     FILTER(?subject = ns1:Identity)
-//
-//     BIND(CONCAT("What information is available about ", STR(ns1:Identity), "?") AS ?query)
-//
-//     ?subject gpt:ask ?query .
-// }
-// ```
+export const getToneOfVoice = (characteristic: string) => {
+    return `PREFIX bk: <http://www.burgerking.com/brand#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX gpt: <http://www.ontotext.com/gpt/>
+
+            SELECT ?characteristicName ?emailCopy WHERE {
+    
+                ${characteristic} a bk:Characteristic ;
+                        rdfs:label ?characteristicName .
+    
+                BIND(CONCAT("Can you provide copy for an email template using the ", ?characteristicName, " tone of voice?") AS ?query)
+                ?emailCopy gpt:ask ?query .
+            }`
+}
